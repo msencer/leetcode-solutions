35e08345fae766ae0ce48498feef5350dc53a172
Mehmet Sencer KARADAYI <msencerkaradayi@gmail.com>
2016-07-29 11:58:43 +0300

    Question added

--- easy/MoveZeroes.py
+++ easy/MoveZeroes.py
@@ -1,3 +1,12 @@
+'''
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+Note:
+    You must do this in-place without making a copy of the array.
+    Minimize the total number of operations.
+'''
 class Solution(object):
     def moveZeroes(self, nums):
         l = len(nums)
@@ -16,8 +25,6 @@ class Solution(object):
                 i+=1
                 j+=1
         return nums
-
-
 
 
 
