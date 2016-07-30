9de1f08212ec027211e8ff56708b733fabfb8add
Mehmet Sencer KARADAYI <msencerkaradayi@gmail.com>
2016-07-30 16:59:26 +0300

    Major Bugfix and added new test cases

--- easy/TwoSum.py
+++ easy/TwoSum.py
@@ -11,11 +11,14 @@ class Solution(object):
             lfor = sys.maxint
             prev = 0
             for i,v in enumerate(nums):
+                sfor = target-v
+                if lfor<sys.maxint and lfor != v:
+                    continue
                 if lfor == v:
                     return [prev,i]
-                res = self.search(s,target-v)
-                if  res>0:
-                   lfor = target-v
+                res = self.search(s,sfor)
+                if  res>=0:
+                   lfor = sfor
                    prev = i
         def search(self,nums,val):
             left,right = 0,len(nums)-1
@@ -34,3 +37,6 @@ class Solution(object):
 s = Solution()
 assert [0,1] == s.twoSum([2,7,11,15],9)
 assert [3,5] == s.twoSum([9,3,4,1,87,2,3],3)
+assert [3,4] == s.twoSum([2,1,9,4,4,56,90,3],8)
+assert [0,3] == s.twoSum([0,3,4,0],0)
+assert [2,4] == s.twoSum([-1,-2,-3,-4,-5],-8)
