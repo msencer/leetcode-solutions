e256b2e94521001422eb915acf6a818d674f516b
msencer <msencerkaradayi@gmail.com>
2016-08-15 17:03:25 +0300

    Number of 1 bits solution added in python

---
+++ easy/python/NumberOf1Bits.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+'''
+Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+
+For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+'''
+class Solution(object):
+    def hammingWeight(self, n):
+        c = 0
+        while n > 0:
+            c += n & 1
+            n >>= 1
+        return c
+
+if __name__== "__main__":
+    assert 3 == Solution().hammingWeight(11)
+    assert 1 == Solution().hammingWeight(1024)
+    assert 10 == Solution().hammingWeight(1023)
